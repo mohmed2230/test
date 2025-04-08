17d3cafab8e630070a1f70f38afea4e155043555
mohmed <mohmedhmdall@gmail.com>
2025-04-08 23:55:13 +0200

    first commit

---
+++ android/app/src/main/kotlin/com/example/datahive/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.datahive
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/MyHomePage.dart
@@ -0,0 +1,38 @@
+
+
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            // Add a Text widget to display the counter value
+            const Text(
+              '0', // Replace with your counter variable
+              style: TextStyle(fontSize: 24),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}

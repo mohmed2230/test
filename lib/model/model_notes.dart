07548e23f9db6255bc17619baba6b6a91ee9a562
mohmed <mohmedhmdall@gmail.com>
2025-04-09 15:40:02 +0200

    add packages

---
+++ lib/model/model_notes.dart
@@ -0,0 +1,18 @@
+class ModelNotes {
+  final String title;
+  final String subtitle;
+
+  final String content;
+  final String date;
+
+  final String color;
+
+
+  ModelNotes({
+    required this.title,
+    required this.subtitle,
+    required this.content,
+    required this.date,
+    required this.color,
+  });
+}
